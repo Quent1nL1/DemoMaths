c0edfb3d21ea60d3493ddc0329601461f148dc75
Quentin <quentin@lyonnet.org>
2025-07-26 12:50:19 +0200

    Initial commit

---
+++ app.config.js
@@ -0,0 +1,12 @@
+export default {
+  name: "DemoMaths",
+  slug: "DemoMaths",
+  version: "1.0.0",
+  platforms: ["web"],
+  web: {
+    bundler: "metro",
+    output: "static",
+    favicon: "./assets/favicon.png",
+    assetPrefix: "/DemoMaths/"
+  }
+};
